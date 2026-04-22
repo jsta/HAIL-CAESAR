ac6e50e4a8d6be2290f2d2ddb49e52cd4347d2e1
jsta <jemma.stachelek@gmail.com>
2026-04-22 13:59:27 -0600

    linting

--- test/basic_kga_tests.py
+++ test/basic_kga_tests.py
@@ -37,14 +37,18 @@ def rasters_params():
     Pytest fixture that reads in paths containing results/KGAs
     and returns them as a list of params. (For rasters)
     """
-    with open('known_good_answers/rasters.json') as f:
+    with open("known_good_answers/rasters.json") as f:
         fixtures = json.loads(f.read())
     params = []
 
     for fix in fixtures:
-        params.append(pytest.param(raster(fixtures[fix]['result']),
-                                   raster(fixtures[fix]['expected']),
-                                   id=fix))
+        params.append(
+            pytest.param(
+                raster(fixtures[fix]["result"]),
+                raster(fixtures[fix]["expected"]),
+                id=fix,
+            )
+        )
     return params
 
 
@@ -52,24 +56,28 @@ def timeseries_params():
     """
     Fixture for the timeseries files
     """
-    with open('known_good_answers/timeseries.json') as f:
+    with open("known_good_answers/timeseries.json") as f:
         fixtures = json.loads(f.read())
     params = []
 
     for ts in fixtures:
-        params.append(pytest.param(timeseries(fixtures[ts]['result'], 1),
-                                   timeseries(fixtures[ts]['expected'], 1),
-                                   id=ts))
+        params.append(
+            pytest.param(
+                timeseries(fixtures[ts]["result"], 1),
+                timeseries(fixtures[ts]["expected"], 1),
+                id=ts,
+            )
+        )
     return params
 
 
-class TestingHAILCAESAR():
+class TestingHAILCAESAR:
     @staticmethod
-    @pytest.mark.parametrize('result,expected', rasters_params())
+    @pytest.mark.parametrize("result,expected", rasters_params())
     def test_water_depths(result, expected):
         ntest.assert_allclose(result, expected, rtol=1e-03)
 
     @staticmethod
-    @pytest.mark.parametrize('result,expected', timeseries_params())
+    @pytest.mark.parametrize("result,expected", timeseries_params())
     def test_hydrograph_lisflood(result, expected):
         ntest.assert_allclose(result, expected, rtol=1e-03)
